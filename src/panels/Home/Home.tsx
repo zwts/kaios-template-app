96e545a112f3b2e03e7b0a1ed248c78ee25ec2d3
Tan Shi <shi.tan@kaiostech.com>
2024-05-07 16:49:17 +0800

    Add navigation in Home.

--- src/panels/Home/Home.tsx
+++ src/panels/Home/Home.tsx
@@ -1,14 +1,32 @@
-import { Fragment, FunctionalComponent, h } from "preact";
+import { Fragment, FunctionalComponent, createRef, h } from "preact";
+import { useEffect, useRef } from "preact/hooks";
 
 import Button from "../../components/Button";
+import SimpleNavigationHelper from "../../helpers/simple_navigation_helper"
 
 import "./Home.scss";
 
+
 const Home: FunctionalComponent = () => {
+  const panelRef = useRef<HTMLDivElement>(null);
+  const navigatorRef = createRef();
+
+  useEffect(() => {
+    const panel = panelRef.current;
+    if (!panel) {
+      return;
+    }
+
+    if (!navigatorRef.current) {
+      navigatorRef.current = new SimpleNavigationHelper('.pill-button', panel, '.home-container');
+    }
+    setTimeout(() => {console.log('home page focus');panel.focus()})
+    return () => {};
+  }, []);
 
   return (
     <Fragment>
-      <div className="home">
+      <div className="home" ref={panelRef} tabIndex={-1}>
         <div className="home-header" data-l10n-id="reader-or-card"></div>
         <div className="home-container">
           <Button l10nId="reader"></Button>
